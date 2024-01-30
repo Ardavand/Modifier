0ca257d42b9c9f626a68bb344cb0c511b542203b
Ardavand <156684629+Ardavand@users.noreply.github.com>
2024-01-30 11:33:59 +0700

    Add files via upload

---
+++ Modifier.java
@@ -0,0 +1,18 @@
+
+package modifier;
+public class Modifier {
+    public static void main(String[] args) {
+        
+//        PublicPerson parson = new PublicPerson();
+//        parson.name = "Davand";
+        
+        Person mPerson = new Person();
+        mPerson.setName("Ardavand");
+        
+        System.out.println("Person Name : "+ mPerson.getName());
+        
+        
+        
+    }
+    
+}
---
+++ PublicPerson.java
@@ -0,0 +1,13 @@
+
+package modifier;
+public class PublicPerson {
+    String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
